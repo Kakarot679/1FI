fc8b724e99c26901b5a268f1c91a4589f81e5032
Hardik Gupta <hardikgupta15082003@gmail.com>
2026-09-01 21:14:33 +0530

    Initialize Expo + TypeScript project
    Bootstrapped with create-expo-app (blank-typescript). Added ESLint
    (eslint-config-expo flat) + Prettier, tightened tsconfig, and set the
    app name/slug.

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ['dist/*', 'node_modules/*'],
+  },
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+    },
+  },
+]);
